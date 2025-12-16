92a4a76c37299bc3156919aa74e5e530baafe723
Pâmela dos Reis <pammreiss@gmail.com>
2025-12-16 18:35:08 -0300

    Adicionei os testes e2e

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -9,7 +9,9 @@ import {
 import { AuthService } from '../services/auth.service';
 import { UsuarioLogin } from '../entities/usuariologin.entity';
 import { LocalAuthGuard } from '../guard/local-auth.guard';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Usuario')
 @Controller('/usuarios')
 export class AuthController {
   constructor(private authService: AuthService) {}
